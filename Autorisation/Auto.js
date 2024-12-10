64320cdb16b1b4a642a328f0c3f1df9143cda2f6
ural-ECTS <ural@ects.ru>
2024-12-10 13:48:14 +0500

    1

---
+++ Autorisation/Auto.js
@@ -0,0 +1,7 @@
+function toHome(){
+    window.location.href = '../Login/Login.html'
+}
+
+function toReg(){
+    window.location.href = '../Registration/Reg.html'
+}
---
+++ Registration/Reg.js
@@ -0,0 +1,7 @@
+function Reg(){
+    alert('failed')
+}
+
+function toAuto(){
+    window.location.href = '../Autorisation/Auto.html'
+}
